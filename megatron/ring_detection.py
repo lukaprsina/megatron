0f50c320c5c0b9b1bc33a0185e39022a1c5ea429
Luka Pršina <prsinaluka@gmail.com>
2026-03-18 13:30:42 +0100

    first commit

---
+++ megatron/ring_detection.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from megatron.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'megatron'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='iota',
+    maintainer_email='iota@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'ring_detection = megatron.ring_detection:main'
+        ],
+    },
+)
