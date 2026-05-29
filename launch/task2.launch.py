18894756d6d897260f8b05278c778eccdf97f372
Luka Pršina <prsinaluka@gmail.com>
2026-05-29 19:28:21 +0200

    fix

--- launch/task2.launch.py
+++ launch/task2.launch.py
@@ -54,8 +54,7 @@ def generate_launch_description():
         package='rviz2',
         executable='rviz2',
         name='rviz2',
-        arguments=['-d', PathJoinSubstitution(
-            [pkg_megatron, 'config', LaunchConfiguration('rviz_config')])],
+        arguments=['-d', LaunchConfiguration('rviz_config')],
         parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}],
         remappings=[
             ('/tf', 'tf'),
